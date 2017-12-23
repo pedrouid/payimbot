f0ea8fa6b47c426c4d96e8769cef1cdd7e040f5a
Pedro Gomes <pedrogomes94@gmail.com>
2017-12-23 15:26:36 +0000

    post to telegram

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express'); //eslint-disable-line
 const router = require('express-promise-router')();
 const telegram = require('./telegram');
 
-router.route('/telegram').get(telegram);
+router.route('/telegram').post(telegram);
 
 module.exports = router;
